80c0ad934de007c9cf229314024f0345fb2294d6
Tito Esteves <titoesteves@live.com>
2016-01-20 22:57:22 -0800

    interate through arrays using handlebars

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 (function(){
   renderPage();
+  renderDogs();
   function renderPage() {
     var template = $('#index-template').html(),
         compiled = Handlebars.compile(template),
@@ -10,5 +11,12 @@
     $('#languageSwitch').click(function(){
       DogPack.switchLanguage();
     })
+  }
+
+  function renderDogs() {
+    var template = $('#dogs-template').html(),
+      compiled = Handlebars.compile(template),
+      rendered = compiled({dogs: DogPack.dogs, language: window.language});
+    $('#theDogs').html(rendered);
   }
 })();
