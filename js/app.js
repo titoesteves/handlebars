77e5fce1fff16c6ad954304a736b68445445f90a
Tito Esteves <titoesteves@live.com>
2016-01-21 20:59:00 -0800

    added if else logic

--- js/app.js
+++ js/app.js
@@ -18,5 +18,20 @@
       compiled = Handlebars.compile(template),
       rendered = compiled({dogs: DogPack.dogs, language: window.language});
     $('#theDogs').html(rendered);
+    attachDogsButton();
+  }
+
+  function attachDogsButton() {
+    $('.dog-button').click(function(){
+      var id = $(this).closest('.dog-card').data('dog-id')
+      DogPack.chooseDog(id);
+      renderDogs()
+    })
+
+    $('.not-dog-button').click(function(){
+      var id = $(this).closest('.dog-card').data('dog-id')
+      DogPack.chooseNotDog(id);
+      renderDogs()
+    })
   }
 })();
